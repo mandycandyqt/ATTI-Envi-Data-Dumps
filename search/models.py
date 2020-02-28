d49d04d902d8741df2c3e79c226d88a1556849e0
avboltax <avboltax@gmail.com>
2020-02-28 09:10:18 -0500

    crawler

--- search/models.py
+++ search/models.py
@@ -6,7 +6,10 @@ from django.db import models
 class Report(models.Model):
     file_name = models.CharField(max_length=100)
     subject = models.CharField(max_length=100)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
     program_id = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+    categories =
+    url = models.URLField()

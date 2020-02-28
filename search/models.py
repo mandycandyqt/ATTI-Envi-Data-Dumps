6a3b0b6970cf9b5af1c38ef418063e271f0c5f2b
avboltax <avboltax@gmail.com>
2020-02-28 08:33:42 -0500

    initial python env

---
+++ search/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Report(models.Model):
+    file_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+    program_id = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
